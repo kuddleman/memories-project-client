6a37462f5659685dfa131903a4223a0c873e67a5
Kuddleman <edl1155@gmail.com>
2020-12-14 12:13:42 -0800

    add redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+import { useDispatch } from 'react-redux'
+import { getPosts } from './actions/posts'
 import memories from './images/memories.png'
 import Form from './Form/Form'
 import Posts from './Posts/Posts'
@@ -7,6 +9,11 @@ import useStyles from './styles'
 
 const App = () => {
     const classes = useStyles()
+    const dispatch = useDispatch()
+
+    useEffect(()=>{
+        dispatch(getPosts())
+    }, [dispatch])
     return (
         <Container maxidth="lg">
             <AppBar className={classes.appBar} position="statuc" color="inherit">
--- src/Posts/Posts.js
+++ src/Posts/Posts.js
@@ -1,6 +1,6 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import Post from './Post/Post'
-
 import useStyles from './styles'
 const Posts = () => {
     const classes = useStyles()
